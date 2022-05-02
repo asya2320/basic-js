84e3ea4c9e100b562e3d8b9196f3baa8e87a2c18
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-05-02 15:08:35 +0300

    feat: add new code

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members){
+      let name='';
+      members=members.sort();
+      for(let i=0; i<members.length; i++){
+          if(typeof members[i]==='string'){
+              name+=members[i].charAt(0);
+          }
+      }
+      if(name==''){
+          return false;
+      }else{
+          return name;
+      }
+  }
 
 module.exports = {
   createDreamTeam
